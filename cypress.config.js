683eebe056f92a5cb76730401e700351ae25a500
Paulo Oliveira <111186037+p-oliveira7@users.noreply.github.com>
2023-11-14 02:20:26 -0300

    feat: adiciona configuração para o github workflows

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,14 @@
 const { defineConfig } = require("cypress");
+require('dotenv').config();
 
 module.exports = defineConfig({
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    configFile: 'reporter-config.json'
+  },
+  env: {
+    ...process.env,
+  },
   e2e: {
     baseUrl: 'https://serverest.dev',
     setupNodeEvents(on, config) {
